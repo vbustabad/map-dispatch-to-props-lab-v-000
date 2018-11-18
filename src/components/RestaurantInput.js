65f48b858e1bca339a18bb17269a3014ada09b45
Veronica Bustabad <vbustabad@gmail.com>
2018-11-18 05:20:21 +0000

    automatically backed up by learn

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -34,6 +34,7 @@ export class RestaurantInput extends Component {
             type="text"
             onChange={(event) => this.handleOnNameChange(event)}
             id="name"
+            value={this.state.name}
             placeholder="restaurant name" />
         </p>
         <p>
@@ -41,6 +42,7 @@ export class RestaurantInput extends Component {
             type="text"
             onChange={(event) => this.handleOnLocationChange(event)}
             id="location"
+            value={this.state.location}
             placeholder="location" />
         </p>
         <input type="submit" />
